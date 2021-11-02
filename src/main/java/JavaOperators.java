d4de2fb317d04a1c8e0dfcc219a0a07290a4d8ce
souban-git <63713476+souban-git@users.noreply.github.com>
2021-11-03 01:30:19 +0530

    Adding first files -java operators class

---
+++ src/main/java/JavaOperators.java
@@ -0,0 +1,28 @@
+
+public class JavaOperators {
+
+	public static void main(String[] args) {
+		JavaOperators jo = new JavaOperators();
+		jo.displayNum(10, 12);
+
+	}
+
+	
+	public void displayNum(int x,int y){
+		System.out.println(x++);//10 (11)  
+		System.out.println(++x);//12  
+		System.out.println(x--);//12 (11)  
+		System.out.println(--x);//10 
+		System.out.println(x++ + ++x);//10+12=22  
+		System.out.println(~x);//-11 (*-1 -1)
+		System.out.println(x-y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+		int c=20;
+		x=10; y=5;
+		System.out.println(x>y||x++<c);//true || true = true  
+		System.out.println(x);//10 because second condition is not checked  
+		System.out.println(x>y|x++<c);//true | true = true  
+		System.out.println(x);//11 because second condition is checked  
+	}
+}
